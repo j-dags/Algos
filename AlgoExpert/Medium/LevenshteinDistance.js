40145b3db89209400eb0a0ba3d5d49d117c64f0c
j-dags <jon.dagdagan@gmail.com>
2021-03-11 12:13:52 -0600

    3/11/21

--- AlgoExpert/Medium/LevenshteinDistance.js
+++ AlgoExpert/Medium/LevenshteinDistance.js
@@ -28,5 +28,24 @@ function levenshteinDistance(str1, str2) {
 // TIME: O(n*m)
 // SPACE: O(n*m)
 
-// Do not edit the line below.
-exports.levenshteinDistance = levenshteinDistance
+/*
+
+This really hinges on understanding the algorithm. We make a 2D array. Let's look at an example:
+
+DISTANCES
+   _  y  a  b  d
+_  0  1  2  3  4
+a  1  1  1  2  3
+b  2  2  2  1  2
+c  3  3  3  2  2
+
+The horizontal axes represent our initial string. The vertical axes represent the target string. I.e. we're trying to convert initial to target. We approach this dynamically, solving how many moves it takes to convert 1 letter, than 2 letters, 3 letters, etc.
+
+Ex:
+In order to convert ya to a, it will take (1) move.
+In order to convert yab to abc, it will take (2) moves
+
+For any element in matrix DISTANCES:
+if row === col, DISTANCES[row][col] = DISTANCES[row-1][col-1]
+else DISTANCES[row][col] is equal to 1 + the min of its left, top, and top left neighbors
+*/
