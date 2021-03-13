ad22b5dd670ce9ecda12c4d702dffad0f16ca89d
j-dags <jon.dagdagan@gmail.com>
2021-03-13 17:20:47 -0600

    3/13/21

---
+++ educative/Sliding Window/SmallestSubarrWithSum.js
@@ -0,0 +1,25 @@
+function smallestSubArr(s, arr) {
+	let windowStart = 0
+	let windowEnd = 0
+	let windowSum = arr[0]
+	let minLength = Infinity
+
+	while (windowEnd < arr.length) {
+		// Slide window end right while sum is less than target
+		if (windowSum < s) {
+			windowEnd++
+			windowSum += arr[windowEnd]
+		}
+		// Slide window start right while sum is greater than target. Check min length
+		else if (windowSum >= s) {
+			minLength = Math.min(minLength, windowEnd - windowStart + 1)
+			windowSum -= arr[windowStart]
+			windowStart++
+		}
+	}
+	return minLength
+}
+
+smallestSubArr(7, [2, 1, 5, 2, 3, 2])
+smallestSubArr(7, [2, 1, 5, 2, 8])
+smallestSubArr(8, [3, 4, 1, 1, 6])
