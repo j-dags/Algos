a32ac1d3087bbff9fecaa6a4bba0469d615a464b
j-dags <jon.dagdagan@gmail.com>
2021-04-13 19:17:16 -0500

    4/13/21

---
+++ Udemy-MasteringFang/TwoSum.js
@@ -0,0 +1,18 @@
+/*
+Questions:
+Are all numbers positive or are there negatives?
+Are there duplicates in the array?
+Will there always be a solution? If not, what should we return?
+Can there be multiple pairs that add up to target value?
+
+Approach:
+  Test Cases
+    [1,3,7,9,2], target = 11, answer= [3,4]
+    [1,3,7,9,2], target = 25, answer= null
+    [], target=1, answer=null
+    [4], target=1, answer=null
+    [1,6], target=7, answer=[0,1]
+
+  Pseudocode: Figure it out prior to coding
+
+*/
