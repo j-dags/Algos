a1d4b523c83a0effde4614ecb585b2b1e8b67d90
j-dags <jon.dagdagan@gmail.com>
2021-05-06 16:56:48 -0500

    5/6/21

--- fsa/doesPathExists.js
+++ fsa/doesPathExists.js
@@ -4,27 +4,38 @@ Given a directed graph. Determine whether a path exists between two given nodes.
 Approach: Implement BFS and DFS to traverse graph.
 */
 
+// BFS TRAVERSAL
 const doesPathExist = (graph, start, target, visited = {}) => {
-	if (!graph[start]) return false // Return false if empty graph
+	if (!graph[start]) return false /// Return false if no edges
 	let queue = [start]
 
-	// BFS traversal
-	while (queue.length > 0) {
-		// Shift node out of the queue
-		let node = queue.shift()
-		// Mark current node as visited
-		visited[node] = true
-		// Check the connected vertices
-		let neighbors = graph[node]
+	while (queue.length) {
+		let node = queue.shift() // Shift node out of the queue
+		visited[node] = true // Mark current node as visited
+		let vertices = graph[node] // Check the connected vertices
 
-		for (const neighbor of neighbors) {
-			// Found our path, return true
-			if (neighbor === target) return true
-			// Only push unvisited nodes/vertices to the queue
-			if (!visited[neighbor]) queue.push(neighbor)
+		for (const neighbor of vertices) {
+			if (neighbor === target) return true // Found our path, return true
+			if (!visited[neighbor]) queue.push(neighbor) // Only push unvisited nodes/vertices to the queue
 		}
 	}
 	return false
+}
+
+// DFS TRAVERSAL
+const doesPathExistDFS = (graph, start, target, visited = {}) => {
+	if (!graph[start]) return false // Return false if no edgesz
+	visited[start] = true // Mark current node as visited
+
+	// .some() will invoke the callback function on each child vertex. It will return true if any of the vertices return true
+	return graph[start].some((vertex) => {
+		if (vertex === target) return true // Return true if target found
+
+		// If a vertex is unvisited, recursively call doesPathExist
+		if (!visited[vertex]) return doesPathExist(graph, vertex, target, visited)
+
+		return false // Otherwise return false
+	})
 }
 
 const graph1 = {
@@ -41,12 +52,12 @@ const graph2 = {
 	d: [],
 }
 
-console.log(doesPathExist(graph1, 'a', 'a')) // true
-console.log(doesPathExist(graph1, 'c', 'c')) // true
-console.log(doesPathExist(graph1, 'r', 's')) // true
-console.log(doesPathExist(graph1, 's', 'a')) // false
+console.log(doesPathExistDFS(graph1, 'a', 'a')) // true
+console.log(doesPathExistDFS(graph1, 'c', 'c')) // true
+console.log(doesPathExistDFS(graph1, 'r', 's')) // true
+console.log(doesPathExistDFS(graph1, 's', 'a')) // false
 
-console.log(doesPathExist(graph2, 'a', 'b')) // true
-console.log(doesPathExist(graph2, 'b', 'a')) // false
-console.log(doesPathExist(graph2, 'a', 'd')) // true
-console.log(doesPathExist(graph2, 'a', 'a')) // false
+console.log(doesPathExistDFS(graph2, 'a', 'b')) // true
+console.log(doesPathExistDFS(graph2, 'b', 'a')) // false
+console.log(doesPathExistDFS(graph2, 'a', 'd')) // true
+console.log(doesPathExistDFS(graph2, 'a', 'a')) // false
