88af82347096352346025e00bfba681779abb267
j-dags <jon.dagdagan@gmail.com>
2021-07-13 17:12:06 -0500

    removeIslands FAST

--- interviews/amazon/graphs/removeIslands.js
+++ interviews/amazon/graphs/removeIslands.js
@@ -1,18 +1,75 @@
 /*
 Given a matrix of 1s and 0s, remove all islands (vertically/horizontally adjacent 1s) that don't touch the edges of the matrix.
 
-Approach:
-- Iterate through matrix
-- If we encounter a 1, check if it touches the border (using DFS)
-- If it doesn't touch border, DFS remove the island
-- As we go along, keep track of visited nodes so we don't recurse infinitely
-
-O(wh)t | O(wh)s
-
-
 */
 
-// TIME: 270ms
+// ----------------------------------------------
+// BETTER APPROACH:
+// Go around outsides and find invalid islands. Then iterate inside and remove valid islands
+// No additional data structures needed
+// O(wh)t | O(wh)s. WORSE CASE The recursive stack is the entire matrix.
+// TIME: .234ms. Literally like 25x faster than our other approach.
+// ----------------------------------------------
+function removeIslands(matrix) {
+	// Iterate over matrix and save any invalid islands
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[0].length; j++) {
+			const isEdge =
+				i === 0 ||
+				i === matrix.length - 1 ||
+				j === 0 ||
+				j === matrix[0].length - 1
+
+			if (matrix[i][j] === 1 && isEdge) {
+				dfsEdge(matrix, i, j)
+			}
+		}
+	}
+
+	// Iterate over matrix and remove any islands that are false in edgeIsland matrix
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[0].length; j++) {
+			if (matrix[i][j] === 1) matrix[i][j] = 0
+			if (matrix[i][j] === -1) matrix[i][j] = 1
+		}
+	}
+	return matrix
+}
+
+function dfsEdge(matrix, y, x) {
+	matrix[y][x] = -1
+
+	const adjacent = [
+		[y + 1, x],
+		[y - 1, x],
+		[y, x + 1],
+		[y, x - 1],
+	]
+
+	for (const [dy, dx] of adjacent) {
+		if (matrix[dy] && matrix[dy][dx] === 1) dfsEdge(matrix, dy, dx)
+	}
+}
+
+const matrix = [
+	[1, 0, 0, 0, 0, 0],
+	[0, 1, 0, 1, 1, 1],
+	[0, 0, 1, 0, 1, 0],
+	[1, 1, 0, 0, 1, 0],
+	[1, 0, 1, 1, 0, 0],
+	[1, 0, 0, 0, 0, 1],
+]
+
+console.log(removeIslands1(matrix))
+
+// ----------------------------------------------
+// ORIGINAL APPROACH:
+// Iterate through matrix
+// DFS and find edge islands.
+// Iterate and change non edge islands to 0s
+// TIME: 5ms
+// ----------------------------------------------
+
 function removeIslands1(matrix) {
 	console.time()
 	let visited = {}
@@ -67,68 +124,3 @@ function remIsland(matrix, y, x) {
 		if (matrix[dy] && matrix[dy][dx] === 1) remIsland(matrix, dy, dx)
 	}
 }
-
-// APROACH 2:
-// Go around outsides and find invalid islands. Then iterate inside and remove valid islands
-// TIME: 5ms
-function removeIslands(matrix) {
-	console.time()
-	const edgeIslands = [
-		...Array(matrix.length)
-			.fill(0)
-			.map((el) => Array(matrix[0].length).fill(false)),
-	]
-	const visited = {}
-
-	// Iterate over matrix and save any invalid islands
-	for (let i = 0; i < matrix.length; i++) {
-		for (let j = 0; j < matrix[0].length; j++) {
-			const isEdge =
-				i === 0 ||
-				i === matrix.length - 1 ||
-				j === 0 ||
-				j === matrix[0].length - 1
-
-			if (matrix[i][j] === 1 && isEdge && !visited[`${i},${j}`]) {
-				dfsEdge(matrix, i, j, visited, edgeIslands)
-				visited[`${i},${j}`] = true
-			}
-		}
-	}
-	// Iterate over matrix and remove any islands that are false in edgeIsland matrix
-	for (let i = 0; i < matrix.length; i++) {
-		for (let j = 0; j < matrix[0].length; j++) {
-			if (matrix[i][j] === 1 && edgeIslands[i][j] === false) matrix[i][j] = 0
-		}
-	}
-	console.timeEnd()
-	return matrix
-}
-
-function dfsEdge(matrix, y, x, visited, edgeIslands) {
-	visited[`${y},${x}`] = true
-	edgeIslands[y][x] = true
-
-	const adjacent = [
-		[y + 1, x],
-		[y - 1, x],
-		[y, x + 1],
-		[y, x - 1],
-	]
-
-	for (const [dy, dx] of adjacent) {
-		if (matrix[dy] && matrix[dy][dx] === 1 && !visited[`${dy},${dx}`])
-			dfsEdge(matrix, dy, dx, visited, edgeIslands)
-	}
-}
-
-const matrix = [
-	[1, 0, 0, 0, 0, 0],
-	[0, 1, 0, 1, 1, 1],
-	[0, 0, 1, 0, 1, 0],
-	[1, 1, 0, 0, 1, 0],
-	[1, 0, 1, 1, 0, 0],
-	[1, 0, 0, 0, 0, 1],
-]
-
-console.log(removeIslands1(matrix))
