a8fbcbc77a6ac94c555586eb5ac5a95c2a9f0dda
j-dags <jon.dagdagan@gmail.com>
2021-07-14 12:40:12 -0500

    validTree union find

--- interviews/amazon/graphs/261_validTree.js
+++ interviews/amazon/graphs/261_validTree.js
@@ -2,18 +2,67 @@
 You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges where edges[i] = [ai, bi] indicates that there is an undirected edge between nodes ai and bi in the graph.
 
 Return true if the edges of the given graph make up a valid tree, and false otherwise.
-
-Approach:
-Build a graph given the edges. Each key is a node and its value is its edges. DFS recurse through the tree and populate a visited object.
-
-IF there are not precisely n-1 edges, return false
-IF there are no edges, return true
-IF the number of visited nodes === n, return true
-
-O(n)t | O(n)s
 */
 
-var validTree = function (n, edges) {
+/* ----------------------------
+Union find Implementation
+- Create a root array to keep track of representative/root nodes for each node
+- For each edge in edges, find the roots of the start/end nodes. Set the idx of the end note in roots equal to the root of the start node.
+			i.e. roots[end] = start
+- Iterate from 0 to n-1. For each i, find the root node and add it to a set
+- If there are more than 1 element in set, that means graph/tree is not fully connected.
+
+O(n)t | O(n)s
+---------------------------- */
+
+const validTree = (n, edges) => {
+	if (edges.length !== n - 1) return false
+	if (n === 1) return true
+	let roots = [...Array(n).keys()]
+
+	// find helper function
+	const find = (x) => {
+		if (roots[x] !== x) return find(roots[x])
+		return x
+	}
+
+	// union helper function
+	const union = ([a, b]) => {
+		const start = find(a)
+		const end = find(b)
+		roots[end] = start
+	}
+
+	const set = new Set()
+	edges.forEach(union)
+
+	for (let i = 0; i < n; i++) {
+		let root = find(i)
+		set.add(root)
+	}
+
+	return set.size === 1
+}
+
+let input = [
+	[0, 1],
+	[0, 2],
+	[0, 3],
+	[1, 4],
+]
+
+console.log('UF > ', validTree(5, input))
+
+/* ----------------------------
+DFS Implementation
+- Create an adjacency list from the list of edges
+- Starting with node 0, DFS through adjacency list. Keeping track of visited nodes as we go
+- If number of visited nodes !== n then we don't have a connected tree
+
+O(n)t | O(n)s
+---------------------------- */
+var validTreeDFS = function (n, edges) {
+	console.time()
 	if (edges.length !== n - 1) return false
 	if (n === 1) return true
 
@@ -41,15 +90,8 @@ var validTree = function (n, edges) {
 	}
 
 	dfs(0)
+	console.timeEnd()
 	return visited.size === n
 }
 
-let input = [
-	[0, 1],
-	[1, 2],
-	[2, 3],
-	[1, 3],
-	[1, 4],
-]
-
-console.log(validTree(5, input))
+console.log('DFS > ', validTreeDFS(5, input))
